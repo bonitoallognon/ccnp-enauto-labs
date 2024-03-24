eccd22282ca90560ee08db18f9fde20093147a14
Bonito Allognon <allognonb@yahoo.fr>
2024-03-24 09:11:28 +0100

    add conditionals

---
+++ python code 1/conditionals.py
@@ -0,0 +1,13 @@
+direction = input("Do you want to go (N)orth, (S)outh, (E)ast, or (W)est?") 
+
+if direction=="n":
+    print("You head north, into the forest.") 
+elif direction=="s":
+    print("The coast blocks your path south.") 
+elif direction=="s":
+    print("The western fields are comforming to walk trough.") 
+elif direction=="e":
+    print("You were eateb by a grue.") 
+else :
+    print("I didn't recognize your choice.") 
+
